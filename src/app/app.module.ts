3ed68404818a8481eebdef9ff4a13e3e7e8ddb7b
Tsai-Chieh <d0177381@gmail.com>
2023-09-02 23:35:27 +0800

    :tada: ng g  c component/addassociate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddassociateComponent } from './component/addassociate/addassociate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddassociateComponent
   ],
   imports: [
     BrowserModule,
