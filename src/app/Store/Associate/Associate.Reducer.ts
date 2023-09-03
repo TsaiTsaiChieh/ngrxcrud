6a06b997837578f99fe3c374f399efad11af12ec
Tsai-Chieh <d0177381@gmail.com>
2023-09-03 19:48:12 +0800

    :tada: init _AssociateReducer

---
+++ src/app/Store/Associate/Associate.Reducer.ts
@@ -0,0 +1,16 @@
+// https://ngrx.io/guide/store/reducers
+
+import { createReducer, on } from '@ngrx/store';
+import { AssociateState } from './Associate.State';
+import { loadassociatefail, loadassociatesuccess } from './Associate.Action';
+
+const _AssociateReducer = createReducer(
+  AssociateState,
+  on(loadassociatesuccess, (state, action) => {
+    return { ...state, list: [...action.list], errormessage: '' };
+  }),
+  on(loadassociatefail, (state, action) => {
+    return { ...state, list: [], errormessage: action.errormessage };
+  })
+);
+export function AssociateReducer(state: any, action: any) {}
