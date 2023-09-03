8172ca0db1176fbeb8fbb02a60a04ca7c7037e70
Tsai-Chieh <d0177381@gmail.com>
2023-09-03 19:02:50 +0800

    :pencil2: add AssociateModel for state

--- src/app/Store/Model/Associate.model.ts
+++ src/app/Store/Model/Associate.model.ts
@@ -3,7 +3,14 @@ export interface Associates {
   name: string;
   email: string;
   phone: string;
+  type: string;
   address: string;
   associategrop: string;
   status: boolean;
+}
+
+export interface AssociateModel {
+  list: Associates[];
+  associateobj: Associates;
+  errormessage: string;
 }
