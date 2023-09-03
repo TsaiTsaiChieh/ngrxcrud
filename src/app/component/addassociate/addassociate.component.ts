7987d2a31173ae4da8c4c6cdb2c47245b405db11
Tsai-Chieh <d0177381@gmail.com>
2023-09-03 15:43:49 +0800

    :sparkles: associateForm logic

--- src/app/component/addassociate/addassociate.component.ts
+++ src/app/component/addassociate/addassociate.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addassociate',
   templateUrl: './addassociate.component.html',
-  styleUrls: ['./addassociate.component.css']
+  styleUrls: ['./addassociate.component.css'],
 })
-export class AddassociateComponent {
+export class AddassociateComponent implements OnInit {
+  constructor(private builder: FormBuilder) {}
+  ngOnInit(): void {}
+  title = 'Create Associate';
+  associateForm = this.builder.group({
+    id: this.builder.control(2),
+    name: this.builder.control('', Validators.required),
+    email: this.builder.control('', [Validators.required, Validators.email]),
+    phone: this.builder.control('', Validators.required),
+    address: this.builder.control('', Validators.required),
+    type: this.builder.control('Custom'),
+    group: this.builder.control('level1'),
+    status: this.builder.control(true),
+  });
 
+  SaveAssociate() {
+    if (this.associateForm.valid) {
+    }
+  }
 }
