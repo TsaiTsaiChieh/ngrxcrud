c0ae57d1cd2440654626ac8de613d8329ef73a3a
Tsai-Chieh <d0177381@gmail.com>
2023-09-03 11:23:01 +0800

    :tada: init MaterialModule and import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddassociateComponent } from './component/addassociate/addassociate.component';
+import {MaterialModule} from './Material.Module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AddassociateComponent } from './component/addassociate/addassociate.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
