24255751ea707158d683281510889e8763babf25
Tsai-Chieh <d0177381@gmail.com>
2023-09-03 12:25:55 +0800

    :truck: ng g c component/associatelisting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddassociateComponent } from './component/addassociate/addassociate.component';
 import { MaterialModule } from './Material.Module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AssociatelistingComponent } from './component/associatelisting/associatelisting.component';
 
 @NgModule({
-  declarations: [AppComponent, AddassociateComponent],
+  declarations: [AppComponent, AssociatelistingComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
