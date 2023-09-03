1f290a65354a4eb943b5793428e314780bbd1aa0
Tsai-Chieh <d0177381@gmail.com>
2023-09-03 11:25:16 +0800

    :sparkles: import HttpClientModule, ReactiveFormsModule and format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddassociateComponent } from './component/addassociate/addassociate.component';
-import {MaterialModule} from './Material.Module';
+import { MaterialModule } from './Material.Module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddassociateComponent
-  ],
+  declarations: [AppComponent, AddassociateComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
