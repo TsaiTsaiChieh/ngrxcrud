1646251cf8664f39085efb613439a301a0fb33de
Tsai-Chieh <d0177381@gmail.com>
2023-09-03 20:04:26 +0800

    :sparkles: add Associatelist logic

--- src/app/component/associatelisting/associatelisting.component.ts
+++ src/app/component/associatelisting/associatelisting.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AddassociateComponent } from '../addassociate/addassociate.component';
 import { MatDialog } from '@angular/material/dialog';
+import { Associates } from 'src/app/Store/Model/Associate.model';
+import { Store } from '@ngrx/store';
+import { getassociatelist } from 'src/app/Store/Associate/Associate.Selectors';
+import { loadassociate } from 'src/app/Store/Associate/Associate.Action';
 
 @Component({
   selector: 'app-associatelisting',
@@ -8,8 +12,15 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./associatelisting.component.css'],
 })
 export class AssociatelistingComponent implements OnInit {
-  constructor(private dialog: MatDialog) {}
-  ngOnInit(): void {}
+  Associatelist!: Associates[];
+  constructor(private dialog: MatDialog, private store: Store) {}
+  ngOnInit(): void {
+    this.store.dispatch(loadassociate());
+    this.store.select(getassociatelist).subscribe((item) => {
+      this.Associatelist = item;
+      console.log(item);
+    });
+  }
 
   FunctionAdd() {
     this.OpenPopup(0, 'Create Associate');
