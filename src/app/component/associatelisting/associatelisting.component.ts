7d246690a5d76094dad227e1ca19c8d1eaf637c3
Tsai-Chieh <d0177381@gmail.com>
2023-09-03 13:29:25 +0800

    :sparkles: add Openpopup func

--- src/app/component/associatelisting/associatelisting.component.ts
+++ src/app/component/associatelisting/associatelisting.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AddassociateComponent } from '../addassociate/addassociate.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-associatelisting',
   templateUrl: './associatelisting.component.html',
-  styleUrls: ['./associatelisting.component.css']
+  styleUrls: ['./associatelisting.component.css'],
 })
-export class AssociatelistingComponent {
+export class AssociatelistingComponent implements OnInit {
+  constructor(private dialog: MatDialog) {}
+  ngOnInit(): void {}
 
+  FunctionAdd() {
+    this.OpenPopup(0, 'Create Associate');
+  }
+
+  OpenPopup(code: number, title: string) {
+    // https://material.angular.io/components/dialog/examples
+    this.dialog.open(AddassociateComponent, {
+      width: '50%',
+      enterAnimationDuration: '500ms',
+      exitAnimationDuration: '50ms',
+      data: {
+        code,
+        title,
+      },
+    });
+  }
 }
