96edd85ffa756e7db82a17600d7362403a9d3278
Tsai-Chieh <d0177381@gmail.com>
2023-09-04 17:47:40 +0800

    :sparkles: add addassociatesuccess action

--- src/app/Store/Associate/Associate.Reducer.ts
+++ src/app/Store/Associate/Associate.Reducer.ts
@@ -2,7 +2,11 @@
 
 import { createReducer, on } from '@ngrx/store';
 import { AssociateState } from './Associate.State';
-import { loadassociatefail, loadassociatesuccess } from './Associate.Action';
+import {
+  addassociatesuccess,
+  loadassociatefail,
+  loadassociatesuccess,
+} from './Associate.Action';
 
 const _AssociateReducer = createReducer(
   AssociateState,
@@ -11,8 +15,14 @@ const _AssociateReducer = createReducer(
   }),
   on(loadassociatefail, (state, action) => {
     return { ...state, list: [], errormessage: action.errormessage };
+  }),
+  on(addassociatesuccess, (state, action) => {
+    const _maxid = Math.max(...state.list.map((o) => o.id));
+    const _newdata = { ...action.inputdata };
+    _newdata.id = _maxid + 1;
+    return { ...state, list: [...state.list, _newdata], errormessage: '' };
   })
 );
 export function AssociateReducer(state: any, action: any) {
-  return _AssociateReducer(state, action)
+  return _AssociateReducer(state, action);
 }
