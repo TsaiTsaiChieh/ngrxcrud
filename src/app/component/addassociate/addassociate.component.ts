43a21eafce4f707c701269397eb4fcb7773f4b64
Tsai-Chieh <d0177381@gmail.com>
2023-09-04 19:06:40 +0800

    :sparkles: update SaveAssociate func

--- src/app/component/addassociate/addassociate.component.ts
+++ src/app/component/addassociate/addassociate.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { addassociate } from 'src/app/Store/Associate/Associate.Action';
+import { Associates } from 'src/app/Store/Model/Associate.model';
 
 @Component({
   selector: 'app-addassociate',
@@ -14,8 +17,10 @@ export class AddassociateComponent implements OnInit {
   constructor(
     private builder: FormBuilder,
     private ref: MatDialogRef<AddassociateComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private store: Store
   ) {}
+
   ngOnInit(): void {
     this.diagdata = this.data;
     this.title = this.diagdata.title;
@@ -30,13 +35,26 @@ export class AddassociateComponent implements OnInit {
     email: this.builder.control('', [Validators.required, Validators.email]),
     phone: this.builder.control('', Validators.required),
     address: this.builder.control('', Validators.required),
-    type: this.builder.control('Custom'),
+    type: this.builder.control('CUSTOMER'),
     group: this.builder.control('level1'),
     status: this.builder.control(true),
   });
 
   SaveAssociate() {
     if (this.associateForm.valid) {
+      const _obj: Associates = {
+        id: this.associateForm.value.id as number,
+        name: this.associateForm.value.name as string,
+        email: this.associateForm.value.email as string,
+        phone: this.associateForm.value.phone as string,
+        associategroup: this.associateForm.value.group as string,
+        address: this.associateForm.value.address as string,
+        type: this.associateForm.value.type as string,
+        status: this.associateForm.value.status as boolean,
+      };
+      this.store.dispatch(addassociate({ inputdata: _obj }));
+
+      this.ClosePopup();
     }
   }
 }
