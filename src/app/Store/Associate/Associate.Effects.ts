190cd0d6b51dc5f3fdbfd009c8a865f17d054732
Tsai-Chieh <d0177381@gmail.com>
2023-09-04 17:46:54 +0800

    :sparkles: add _addasscoiate effect

--- src/app/Store/Associate/Associate.Effects.ts
+++ src/app/Store/Associate/Associate.Effects.ts
@@ -3,9 +3,15 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AssociateService } from 'src/app/service/associate.service';
-import { loadassociate, loadassociatefail } from './Associate.Action';
+import {
+  addassociate,
+  addassociatesuccess,
+  loadassociate,
+  loadassociatefail,
+} from './Associate.Action';
 import { exhaustMap, map, of, catchError } from 'rxjs';
 import { loadassociatesuccess } from './Associate.Action';
+import { showalert } from '../Common/App.Action';
 
 @Injectable()
 export class AssociateEffects {
@@ -19,6 +25,27 @@ export class AssociateEffects {
           }),
           catchError((_error) =>
             of(loadassociatefail({ errormessage: _error.message }))
+          )
+        )
+      )
+    )
+  );
+
+  _addasscoiate = createEffect(() =>
+    this.actions$.pipe(
+      ofType(addassociate),
+      exhaustMap((action) =>
+        this.service.Create(action.inputdata).pipe(
+          map((data) => {
+            return addassociatesuccess({ inputdata: action.inputdata });
+          }),
+          catchError((_error) =>
+            of(
+              showalert({
+                message: 'Failed to create associate',
+                resulttype: 'fail',
+              })
+            )
           )
         )
       )
