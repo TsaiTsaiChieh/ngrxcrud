d39aefc61b8fc7adfc289d3bfad9ce3557a6a281
Tsai-Chieh <d0177381@gmail.com>
2023-09-04 16:10:14 +0800

    :sparkles: add ADD_ASSOCIATE, ADD_ASSOCIATE_SUCCESS actions

--- src/app/Store/Associate/Associate.Action.ts
+++ src/app/Store/Associate/Associate.Action.ts
@@ -6,6 +6,8 @@ import { Associates } from '../Model/Associate.model';
 export const LOAD_ASSOCIATE = '[associate page] load associate';
 export const LOAD_ASSOCIATE_SUCCESS = '[associate page] load associate success';
 export const LOAD_ASSOCIATE_FAIL = '[associate page] load associate fail';
+export const ADD_ASSOCIATE = '[associate page] add associate success';
+export const ADD_ASSOCIATE_SUCCESS = '[associate page] add associate success';
 
 export const loadassociate = createAction(LOAD_ASSOCIATE);
 export const loadassociatesuccess = createAction(
@@ -15,4 +17,12 @@ export const loadassociatesuccess = createAction(
 export const loadassociatefail = createAction(
   LOAD_ASSOCIATE_FAIL,
   props<{ errormessage: string }>()
+);
+export const addassociate = createAction(
+  ADD_ASSOCIATE,
+  props<{ inputdata: Associates }>()
+);
+export const addassociatesuccess = createAction(
+  ADD_ASSOCIATE_SUCCESS,
+  props<{ inputdata: Associates }>()
 );
